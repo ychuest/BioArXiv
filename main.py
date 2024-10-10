c878ea2d9c54d08b0f0b10c5b24dcc5798f5e8b9
Cheng Yang <yangchengyjs@163.com>
2024-10-10 09:15:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ with open("README.md", "r") as f:
     if last_update_date == current_date:
         sys.exit("Already updated today!")
 
-keywords = ["DNA", "Sequences", "Large", "Foundation", "DNA Sequences", "DNA Language Model", "Genomic Language Model"] # TODO add more keywords
+# keywords = ["DNA", "Sequences", "Large", "Foundation", "DNA Sequences", "DNA Language Model", "Genomic Language Model"] # TODO add more keywords
+keywords = ["DNA", "Sequences", "Large", "Foundation"] 
 
 max_result = 100 # maximum query results from arXiv API for each keyword
 issues_result = 15 # maximum papers to be included in the issue
